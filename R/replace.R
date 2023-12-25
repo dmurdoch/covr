508f71fc5c1bdafb223f10bb6a22f5e24e3f7e57
Duncan Murdoch <murdoch.duncan@gmail.com>
2023-12-25 15:52:47 -0500

    Recursively search lists in environments for code that counts for coverage.

--- R/replace.R
+++ R/replace.R
@@ -24,13 +24,33 @@ replacement <- function(name, env = as.environment(-1), target_value = get(name,
       target_value = target_value,
       new_value = new_value
     )
+  } else if (is.list(target_value)) {
+    replacements_from_list(name, env, target_value)
   }
 }
 
 replace <- function(replacement) {
-  .Call(covr_reassign_function, replacement$name, replacement$env, replacement$target_value, replacement$new_value)
+  if (inherits(replacement, "covr_list")) {
+    replace_or_reset_list(replace, replacement)
+  } else
+    .Call(covr_reassign_function, replacement$name, replacement$env, replacement$target_value, replacement$new_value)
 }
 
 reset <- function(replacement) {
-  .Call(covr_reassign_function, replacement$name, replacement$env, replacement$target_value, replacement$orig_value)
+  if (inherits(replacement, "covr_list")) {
+    replace_or_reset_list(reset, replacement)
+  } else
+    .Call(covr_reassign_function, replacement$name, replacement$env, replacement$target_value, replacement$orig_value)
+}
+
+replace_or_reset_list <- function(theupdate, replacement) {
+  lapply(replacement$replacements, theupdate)
+  env <- replacement$env
+  name <- replacement$name
+  tempenv <- replacement$tempenv
+  for (i in ls(tempenv))
+    eval(substitute(thelist[[i]] <- value,
+                    list(thelist = as.name(name),
+                         i = as.numeric(i),
+                         value = get(i, envir = tempenv))), envir = env)
 }
